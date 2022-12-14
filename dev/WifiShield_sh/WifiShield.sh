93f39c179f0de2051da225577e7a0c48c0bcefaa
Breadn <bylueang@gmail.com>
2022-12-13 21:43:16 -0600

    work

--- dev/WifiShield_sh/WifiShield.sh
+++ dev/WifiShield_sh/WifiShield.sh
@@ -40,9 +40,8 @@ main ()
                       <                          >
                     <      WiFi-Shield v0.0.1      >
                       <                          >
-                        ========================
-            
-                            ($SSID)
+                        ======================== 
+Shielding: ($SSID)
             \n\n\n\n\n\n"
     echo "Welcome! Select one of the following actions..."
     printl
